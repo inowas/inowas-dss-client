d1ac23c83ae5212555368bb32e0764e3484cc979
Martin Wudenka <Martin.Wudenka@gmx.de>
2016-12-09 15:19:38 +0100

    Fix small layout bugs
     - add margin-left to labels
     - add border-spacing to tool
     - moved textures to images

--- src/js/pages/Tool.js
+++ src/js/pages/Tool.js
@@ -1,21 +1,18 @@
 import React from "react"
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 
 import ModflowMap from "./ModFlowMap";
 
 import * as modflowAction from "../actions/ModelActions"
 import * as appAction from "../actions/ApplicationActions"
 
-
 @connect((store) => {
-    return {
-        models: store.models,
-    }
+    return {models: store.models}
 })
 export default class Tool extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
 
             <div className="page-wrapper">
 
@@ -23,7 +20,7 @@ export default class Tool extends React.Component {
                     <section className="tile col col-abs-2 stacked">
                         <h2>Background</h2>
                         <div className="center-horizontal center-vertical">
-                            <img src="img/diagram.png" />
+                            <img src="images/diagram.png"/>
                         </div>
                     </section>
 
@@ -32,15 +29,17 @@ export default class Tool extends React.Component {
                         <div className="grid-container">
                             <div className="col stretch">
                                 <div className="aspect-ratio-wrapper">
-                                    <div className="aspect-ratio-element diagram">
-
-                                    </div>
+                                    <div className="aspect-ratio-element diagram"></div>
                                 </div>
                             </div>
                             <div className="col col-rel-0-5">
                                 <ul className="nav nav-stacked" role="navigation">
-                                    <li><button className="button">PNG</button></li>
-                                    <li><button className="button">CSV</button></li>
+                                    <li>
+                                        <button className="button">PNG</button>
+                                    </li>
+                                    <li>
+                                        <button className="button">CSV</button>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -53,57 +52,62 @@ export default class Tool extends React.Component {
                         <div className="center-vertical center-horizontal">
                             <div className="radio-group">
                                 <div>
-                                    <input name="radio-group-1" type="radio" value="" id="radio1" />
-                                <label for="radio1">Confined Aquifer</label>
+                                    <input name="radio-group-1" type="radio" value="" id="radio1"/>
+                                    <label for="radio1">Confined Aquifer</label>
+                                </div>
+                                <div>
+                                    <input name="radio-group-1" type="radio" value="" id="radio2"/>
+                                    <label for="radio2">Unconfined Aquifer</label>
+                                </div>
                             </div>
-                            <div>
-                                <input name="radio-group-1" type="radio" value="" id="radio2" />
-                            <label for="radio2">Unconfined Aquifer</label>
                         </div>
+
+                    </section>
+
+                    <section className="tile col col-abs-3 stretch">
+                        <div className="grid-container">
+
+                            <div className="col stretch parameters-wrapper">
+                                <table className="parameters">
+                                    <tbody>
+                                        <tr className="parameter">
+                                            <td className="parameter-label">
+                                                Hydraulic Conductivity, K (m/d)
+                                            </td>
+                                            <td className="">
+                                                <input className="parameter-min input input-xs" type="text"/>
+                                                <input className="parameter-max input input-xs" type="text"/>
+                                                <input className="parameter-input" type="range" min="0" max="30" step="1" defaultValue="15"/>
+                                            </td>
+                                        </tr>
+                                        <tr className="parameter">
+                                            <td className="parameter-label">
+                                                Depth to base of aquifer, B(m)
+                                            </td>
+                                            <td className="parameter-inputs">
+                                                <input className="parameter-min input input-xs" type="text"/>
+                                                <input className="parameter-max input input-xs" type="text"/>
+                                                <input className="parameter-input" type="range" min="0" max="30" step="1" defaultValue="15"/>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div className="col col-rel-0-5">
+                                <ul className="nav nav-stacked" role="navigation">
+                                    <li>
+                                        <button className="button button-accent">Default</button>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                    </section>
                 </div>
+
             </div>
-
-        </section>
-
-            <section className="tile col col-abs-3 stretch">
-                <div className="grid-container">
-                    <div className="col stretch parameters-wrapper">
-                        <table className="parameters">
-                            <tr className="parameter">
-                                <td className="parameter-label">
-                                    Hydraulic Conductivity, K (m/d)
-                                </td>
-                                <td className="">
-                                    <input className="parameter-min input input-xs" type="text" />
-                                    <input className="parameter-max input input-xs" type="text" />
-                                    <input className="parameter-input" type="range" min="0" max="30" step="1" defaultValue="15" />
-                                </td>
-                            </tr>
-                            <tr className="parameter">
-                                <td className="parameter-label">
-                                    Depth to base of aquifer, B(m)
-                                </td>
-                                <td className="parameter-inputs">
-                                    <input className="parameter-min input input-xs" type="text" />
-                                    <input className="parameter-max input input-xs" type="text" />
-                                    <input className="parameter-input" type="range" min="0" max="30" step="1" defaultValue="15" />
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div className="col col-rel-0-5">
-                        <ul className="nav nav-stacked" role="navigation">
-                            <li><button className="button button-accent">Default</button></li>
-                        </ul>
-                    </div>
-                </div>
-
-
-            </section>
-        </div>
-
-        </div>
         )
     }
 }
-
