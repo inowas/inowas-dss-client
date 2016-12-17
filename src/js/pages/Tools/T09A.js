a0749a6cc0f8a6d00eca45d188c2dff6e295dbbd
Ralf Junghanns <ralf.junghanns@gmail.com>
2016-12-17 09:03:42 +0100

    Adapt T09A

--- src/js/pages/Tools/T09A.js
+++ src/js/pages/Tools/T09A.js
@@ -5,7 +5,7 @@ import Background from "../../components/tools/Background"
 import Chart from "../../components/tools/ChartT09A"
 import Parameters from "../../components/tools/Parameters"
 import Settings from "../../components/tools/Settings"
-import { changeSettings, changeParameter } from "../../actions/T09A"
+import {changeSettings, changeParameter} from "../../actions/T09A"
 
 @connect((store) => {
     return {tool: store.T09A}
@@ -13,11 +13,11 @@ import { changeSettings, changeParameter } from "../../actions/T09A"
 export default class T09A extends React.Component {
 
     handleChange = (e) => {
-        if (e.target.name === 'settings'){
+        if (e.target.name === 'settings') {
             this.props.dispatch(changeSettings(e.target.value));
         }
 
-        if (e.target.name.startsWith('parameter')){
+        if (e.target.name.startsWith('parameter')) {
             const param = e.target.name.split("_");
 
             let parameter = {};
@@ -31,26 +31,17 @@ export default class T09A extends React.Component {
     render() {
         return (
             <div className="page-wrapper">
-              <div className="page-width">
-                <div className="grid-container">
-                    <section className="tile col col-abs-2 stacked">
-                        <Background image={this.props.tool.background.image} />
-                    </section>
+                <div className="page-width">
+                    <div className="grid-container">
+                        <section className="tile col col-abs-2 stacked">
+                            <Background image={this.props.tool.background.image}/>
+                        </section>
 
-                    <section className="tile col col-abs-3 stretch">
-                        <Chart data={this.props.tool.chart.data} />
-                    </section>
-                </div>
-
-                <div className="grid-container">
-                    <section className="tile col col-abs-2">
-                        <Settings data={this.props.tool.settings} handleChange={this.handleChange} />
-                    </section>
-
-                    <section className="tile col col-abs-3 stretch">
-                        <Parameters data={this.props.tool.parameters} handleChange={this.handleChange} />
-                    </section>
-                </div>
+                        <section className="tile col col-abs-3 stretch">
+                            <h2>Parameters</h2>
+                            <Parameters data={this.props.tool.parameters} handleChange={this.handleChange}/>
+                        </section>
+                    </div>
                 </div>
             </div>
         )
