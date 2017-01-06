6ab4e6865abdaebb23a56567e4a173b4a368308b
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-01-06 15:53:34 +0100

    Add new Toolbar

--- src/js/pages/Navbar.js
+++ src/js/pages/Navbar.js
@@ -1,32 +1,90 @@
 import React from "react"
 
+import {Link} from "react-router";
+
 export default class NavBar extends React.Component {
 
     render() {
         return (
             <div>
-                <nav className="navbar navbar-default navbar-fixed-top">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="#">Inowas</a>
-                        </div>
-                        <div id="navbar" className="">
-                            <ul className="nav navbar-nav">
-                                <li className="dropdown">
-                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tools <span className="caret"/></a>
+                <header>
+                    <nav className="navbar-top">
+                        <div className="container">
+
+                            <ul className="level_1" role="menubar">
+                                <li className="submenu">
+                                    <a href="">
+                                        <i className="flaticon-settings-6"></i>Dashboard
+                                    </a>
+                                </li>
+                                <li className="submenu">
+                                    <a href="http://wiki.inowas.hydro.tu-dresden.de">
+                                        <i className="flaticon-file"></i>Documentation</a>
+                                </li>
+                                <li className="submenu">
+                                    <a href="">
+                                        <i className="flaticon-database-1"></i>Datasets
+                                        <span>(18)</span>
+                                    </a>
+                                </li>
+                                <li className="submenu">
+                                    <a href="">
+                                        <i className="flaticon-folder-4"></i>Projects
+                                        <span>(1)</span>
+                                    </a>
+                                </li>
+                                <li className="submenu dropdown">
+                                    <a href="" className="dropdown-toggle">
+                                        <i className="flaticon-windows-1"></i>Applications
+                                        <span>(2)</span>
+                                    </a>
                                     <ul className="dropdown-menu">
-                                        <li><a href="/#/tools/T09C">T09c Upconing</a></li>
-                                        <li><a href="/#/modflow/list">T03 Numerical model setup (MODFLOW)</a></li>
-                                        <li><a href="/#/scenarioanalysis/list">T07 Application specific scenarios analyzer</a></li>
+                                        <li>
+                                            <a href="#">A03. Restoration of groundwater levels</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">A05. Assessment of saltwater intrusion</a>
+                                        </li>
+                                        <li className="add-new">
+                                            <a href="#">+ Add application</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li className="submenu dropdown">
+                                    <a href="" className="dropdown-toggle">
+                                        <i className="flaticon-controls-4"></i>
+                                        Tools
+                                        <span>(3)</span>
+                                    </a>
+                                    <ul className="dropdown-menu">
+                                        <li>
+                                            <Link to={'tools/T09C'}>T09c Upconing</Link>
+                                        </li>
+                                        <li>
+                                            <Link to={'tools/modflow/list'}>T03 Numerical model setup (MODFLOW)</Link>
+                                        </li>
+                                        <li>
+                                            <Link to={'tools/scenarioanalysis/list'}>T07 Application specific scenarios analyzer</Link>
+                                        </li>
+                                        <li className="add-new">
+                                            <a href="#">+ Add tool</a>
+                                        </li>
                                     </ul>
                                 </li>
                             </ul>
+
+                            <ul className="navbar-right">
+                                <li>
+                                    <a href="">
+                                        <i className="flaticon-user"></i>Hey, Ralf Junghanns</a>
+                                </li>
+                            </ul>
+
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </header>
                 {this.props.children}
             </div>
         );
     }
-
 }
