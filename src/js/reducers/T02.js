dfc5229664346f41de898a212878daedfc0e2de7
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-02-03 16:48:16 +0100

    Improve T02 so that either x or y can be displayed

--- src/js/reducers/T02.js
+++ src/js/reducers/T02.js
@@ -1,4 +1,3 @@
-import * as calc from '../calculations/T02';
 import applyParameterUpdate from './applyParameterUpdate';
 
 function getInitialState() {
@@ -6,23 +5,17 @@ function getInitialState() {
         background: {
             'image': '/images/tools/T02.png'
         },
-        chart: {
-            data: [],
-            options: {
-                yAxis: {
-                    domain: [0, 'auto']
-                }
-            }
-        },
-        info: {
-            hmax: 0,
+        settings: {
+            variable: 'x'
         },
         parameters: [{
             order: 0,
             id: 'w',
             name: 'Percolation rate, w(m/d)',
             min: 0,
-            validMin: function(x) {return x >= 0},
+            validMin: function (x) {
+                return x >= 0
+            },
             max: 10,
             value: 0.045,
             stepSize: 0.001,
@@ -32,9 +25,11 @@ function getInitialState() {
             id: 'L',
             name: 'Basin length, L (m)',
             min: 0,
-            validMin: function(x) {return x > 0},
+            validMin: function (x) {
+                return x > 0
+            },
             max: 100,
-            value: 20.5,
+            value: 40,
             stepSize: 1,
             decimals: 1
         }, {
@@ -42,9 +37,11 @@ function getInitialState() {
             id: 'W',
             name: 'Basin width, W (m)',
             min: 0,
-            validMin: function(x) {return x > 0},
+            validMin: function (x) {
+                return x > 0
+            },
             max: 100,
-            value: 20.5,
+            value: 20,
             stepSize: 1,
             decimals: 1
         }, {
@@ -52,7 +49,9 @@ function getInitialState() {
             id: 'hi',
             name: 'Initial groundwater Level, hi (m)',
             min: 0,
-            validMin: function(x) {return x >= 0},
+            validMin: function (x) {
+                return x >= 0
+            },
             max: 100,
             value: 35,
             stepSize: 1,
@@ -62,9 +61,13 @@ function getInitialState() {
             id: 'Sy',
             name: 'Specific yield, Sy (-)',
             min: 0.000,
-            validMin: function(x) {return x > 0},
+            validMin: function (x) {
+                return x > 0
+            },
             max: 0.5,
-            validMax: function(x) {return x <= 0.5},
+            validMax: function (x) {
+                return x <= 0.5
+            },
             value: 0.085,
             stepSize: 0.001,
             decimals: 3
@@ -73,9 +76,13 @@ function getInitialState() {
             id: 'K',
             name: 'Hydraulic conductivity, K (m/d)',
             min: 0.1,
-            validMin: function(x) {return x > 0},
+            validMin: function (x) {
+                return x > 0
+            },
             max: 10,
-            validMax: function(x) {return x <= 100000},
+            validMax: function (x) {
+                return x <= 100000
+            },
             value: 1.83,
             stepSize: 0.1,
             decimals: 1
@@ -84,7 +91,9 @@ function getInitialState() {
             id: 't',
             name: 'Infiltration time, t (d)',
             min: 0,
-            validMin: function(x) {return x > 0},
+            validMin: function (x) {
+                return x > 0
+            },
             max: 100,
             value: 1.5,
             stepSize: 1,
@@ -92,71 +101,31 @@ function getInitialState() {
         }]
     }
 };
+
 const T02Reducer = (state = getInitialState(), action) => {
     switch (action.type) {
         case 'RESET_TOOL_T02':
-            {
-                state = getInitialState();
-                calculateAndModifyState(state);
-                break;
-            }
-        case 'CALCULATE_TOOL_T02':
-            {
-                state = { ...state
-                };
-                calculateAndModifyState(state);
-                break;
-            }
+            state = getInitialState();
+            break;
+        case 'CHANGE_TOOL_T02_SETTINGS':
+            state = {
+                ...state,
+                settings: action.payload
+            };
+
+            break;
         case 'CHANGE_TOOL_T02_PARAMETER':
-            {
-                state = { ...state,
-                };
+            state = { ...state,
+            };
 
-                const newParam = action.payload;
-                var param = state.parameters.find(p => {return p.id === newParam.id});
-                applyParameterUpdate(param, newParam);
-
-                calculateAndModifyState(state);
-                break;
-            }
+            const newParam = action.payload;
+            var param = state.parameters.find(p => {
+                return p.id === newParam.id
+            });
+            applyParameterUpdate(param, newParam);
+            break;
     }
     return state;
 };
 
-function calculateAndModifyState(state) {
-    const w = state.parameters.find(p => {
-            return p.id == 'w'
-        })
-        .value;
-    const L = state.parameters.find(p => {
-            return p.id == 'L'
-        })
-        .value;
-    const W = state.parameters.find(p => {
-            return p.id == 'W'
-        })
-        .value;
-    const hi = state.parameters.find(p => {
-            return p.id == 'hi'
-        })
-        .value;
-    const Sy = state.parameters.find(p => {
-            return p.id == 'Sy'
-        })
-        .value;
-    const K = state.parameters.find(p => {
-            return p.id == 'K'
-        })
-        .value;
-    const t = state.parameters.find(p => {
-            return p.id == 't'
-        })
-        .value;
-    state.chart.data = calc.calculateDiagramData(w, L, W, hi, Sy, K, t, 0, 120, 10);
-
-
-    state.info.hmax = state.chart.data[0].hhi + hi;
-    //state.chart.options.yAxis.domain[1] = 2 * calc.calculateZCrit(d);
-    return state
-}
 export default T02Reducer;
--- src/js/reducers/T09D.js
+++ src/js/reducers/T09D.js
@@ -4,30 +4,31 @@ import applyParameterUpdate from './applyParameterUpdate';
 function getInitialState() {
     return {
         background: {
-            'image' : '/images/tools/T09D.png'
+            'image': '/images/tools/T09D.png'
         },
         chart: {
             data: [],
             options: {
                 yAxis: {
-                    domain: [0 , 'auto']
+                    domain: [0, 'auto']
                 }
             }
         },
         settings: {
-            'selected': 'confined'
+            selected: 'confined'
         },
         info: {
             x: 0,
             q: 0
         },
-        parameters: [
-            {
+        parameters: [{
                 order: 0,
                 id: 'k',
                 name: 'Hydraulic conductivity, K (m/d)',
                 min: 1,
-                validMin: function(x) {return x > 0},
+                validMin: function (x) {
+                    return x > 0
+                },
                 max: 100,
                 value: 50,
                 stepSize: 1,
@@ -38,7 +39,9 @@ function getInitialState() {
                 id: 'b',
                 name: 'Aquifer thickness below sea level, b (m)',
                 min: 10,
-                validMin: function(x) {return x > 0},
+                validMin: function (x) {
+                    return x > 0
+                },
                 max: 100,
                 value: 20,
                 stepSize: 1,
@@ -49,7 +52,9 @@ function getInitialState() {
                 id: 'q',
                 name: 'Offshore discharge rate, q (m³/d)',
                 min: 0.1,
-                validMin: function(x) {return x > 0},
+                validMin: function (x) {
+                    return x > 0
+                },
                 max: 10,
                 value: 1,
                 stepSize: 0.1,
@@ -60,7 +65,9 @@ function getInitialState() {
                 id: 'qw',
                 name: 'Well pumping rate, Qw (m³/d)',
                 min: 1000,
-                validMin: function(x) {return x > 0},
+                validMin: function (x) {
+                    return x > 0
+                },
                 max: 8000,
                 value: 5000,
                 stepSize: 10,
@@ -71,7 +78,9 @@ function getInitialState() {
                 id: 'xw',
                 name: 'Distance from well to shoreline, xw (m)',
                 min: 1000,
-                validMin: function(x) {return x > 0},
+                validMin: function (x) {
+                    return x > 0
+                },
                 max: 5000,
                 value: 2000,
                 stepSize: 1,
@@ -82,9 +91,13 @@ function getInitialState() {
                 id: 'rhof',
                 name: 'Density of freshwater [g/cm³]',
                 min: 0.9,
-                validMin: function(x) {return x >= 0.9},
+                validMin: function (x) {
+                    return x >= 0.9
+                },
                 max: 1.03,
-                validMax: function(x) {return x <= 1.05},
+                validMax: function (x) {
+                    return x <= 1.05
+                },
                 value: 1,
                 stepSize: 0.001,
                 decimals: 3
@@ -94,9 +107,13 @@ function getInitialState() {
                 id: 'rhos',
                 name: 'Density of saltwater [g/cm³]',
                 min: 0.9,
-                validMin: function(x) {return x >= 0.9},
+                validMin: function (x) {
+                    return x >= 0.9
+                },
                 max: 1.03,
-                validMax: function(x) {return x <= 1.05},
+                validMax: function (x) {
+                    return x <= 1.05
+                },
                 value: 1.025,
                 stepSize: 0.001,
                 decimals: 3
@@ -105,58 +122,71 @@ function getInitialState() {
     };
 }
 
-const T09DReducer = ( state=getInitialState(), action ) => {
+const T09DReducer = (state = getInitialState(), action) => {
 
     switch (action.type) {
 
-        case 'CHANGE_TOOL_T09D_SETTINGS': {
+        case 'CHANGE_TOOL_T09D_SETTINGS':
             state = {
                 ...state,
                 settings: action.payload
             };
-
             break;
-        }
 
-        case 'RESET_TOOL_T09D': {
-            state = {...state};
+
+        case 'RESET_TOOL_T09D':
+            state = { ...state
+            };
             state = getInitialState();
             calculateAndModifyState(state);
-
             break;
-        }
 
-        case 'CALCULATE_TOOL_T09D': {
-            state = {...state};
+        case 'CALCULATE_TOOL_T09D':
+            state = { ...state
+            };
             calculateAndModifyState(state);
 
             break;
-        }
 
-        case 'CHANGE_TOOL_T09D_PARAMETER': {
-
-            state = {...state};
+        case 'CHANGE_TOOL_T09D_PARAMETER':
+            state = { ...state
+            };
 
             const newParam = action.payload;
-            var param = state.parameters.find(p => {return p.id === newParam.id});
+            var param = state.parameters.find(p => {
+                return p.id === newParam.id
+            });
             applyParameterUpdate(param, newParam);
 
             calculateAndModifyState(state);
             break;
-        }
     }
 
     return state;
 };
 
 function calculateAndModifyState(state) {
-    const k = state.parameters.find( p => {return p.id == 'k'}).value;
-    const b = state.parameters.find( p => {return p.id == 'b'}).value;
-    const q = state.parameters.find( p => {return p.id == 'q'}).value;
-    const qw = state.parameters.find( p => {return p.id == 'qw'}).value;
-    const xw = state.parameters.find( p => {return p.id == 'xw'}).value;
-    const rhof = state.parameters.find( p => {return p.id == 'rhof'}).value;
-    const rhos = state.parameters.find( p => {return p.id == 'rhos'}).value;
+    const k = state.parameters.find(p => {
+        return p.id == 'k'
+    }).value;
+    const b = state.parameters.find(p => {
+        return p.id == 'b'
+    }).value;
+    const q = state.parameters.find(p => {
+        return p.id == 'q'
+    }).value;
+    const qw = state.parameters.find(p => {
+        return p.id == 'qw'
+    }).value;
+    const xw = state.parameters.find(p => {
+        return p.id == 'xw'
+    }).value;
+    const rhof = state.parameters.find(p => {
+        return p.id == 'rhof'
+    }).value;
+    const rhos = state.parameters.find(p => {
+        return p.id == 'rhos'
+    }).value;
     state.chart.data = calc.calculateDiagramData(q, b, k, rhof, rhos, qw, xw, -100, 0, 1);
 
     // state.chart.data = calc.calculateDiagramData(q, k, d, df, ds, 0, 100, 10);
