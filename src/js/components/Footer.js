8b1e698f07d9d33d6b6311cbde39e58b83b205af
Catalinuss <catalin.stefan@tu-dresden.de>
2017-03-09 22:41:23 +0100

    Updates footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -12,15 +12,22 @@ export default class Slider extends Component {
                 <div className="content app-width grid-container">
                     <div className="col col-abs-1">
                         <p><Link className="link" to={'/impressum'}>Impressum</Link></p>
+                        <p><a href="https://tu-dresden.de/bu/umwelt/hydro/inowas/project/kontakt" target="_blank">Contact</a></p>
                     </div>
                     <div className="col col-abs-1">
                         <p>Developed by</p>
                         <a href="https://tu-dresden.de/bu/umwelt/hydro/inowas" target="_blank">
-                            <img src="../images/logo-inowas.png" alt="Inowas logo"/>
+                            <img src="../images/logo-inowas.svg" alt="INOWAS logo"/>
                         </a>
                     </div>
                     <div className="col col-abs-1">
-                        <p>Founded by</p>
+                        <p>Supported by</p>
+                        <a href="https://tu-dresden.de/" target="_blank">
+                            <img src="../images/logo-tud.svg" alt="TUD logo"/>
+                        </a>
+                    </div>
+                    <div className="col col-abs-1">
+                        <p>Funded by</p>
                         <a href="https://www.bmbf.de/en/index.html" target="_blank">
                             <img src="../images/logo-bmbf.svg" alt="Federal Ministry of Education and Research logo"/>
                         </a>
