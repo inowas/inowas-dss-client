7a6517e3b8ee73368527fd14c56e04c11e77f7c8
Catalinuss <catalin.stefan@tu-dresden.de>
2017-03-10 07:06:00 +0100

    Update Impressum

--- src/js/pages/Impressum.js
+++ src/js/pages/Impressum.js
@@ -73,15 +73,29 @@ export default class Impressum extends Component {
                     <div className="row top-space">
                         <h2>Impressum</h2>
                         <p className="description">
-                            Es gilt das Impressum der TU Dresden mit folgenden Abweichungen:
+                            Es gilt das <a href="https://tu-dresden.de/impressum" target="_blank">Impressum der TU Dresden</a> mit folgenden Abweichungen:
                         </p>
 
                         <p className="description">
-                            Inhaltlich verantwortlich:<br />
+                            <b>Inhaltlich verantwortlich:</b><br />
+                            Dr. Catalin Stefan<br />
+                            Technische Universität Dresden<br />
+                            Junior Research Group INOWAS<br />
+                            01062 Dresden<br />
+                            Tel.: +49 3501 530044<br />
+                            Fax: +49 3501 530022<br />
+                            Email: catalin.stefan@tu-dresden.de<br />
                         </p>
 
                         <p className="description">
-                            Technischer Betrieb:<br />
+                            <b>Technischer Betrieb:</b><br />
+                            Dipl.-Ing. Ralf Junghanns<br />
+                            Technische Universität Dresden<br />
+                            Junior Research Group INOWAS<br />
+                            01062 Dresden<br />
+                            Tel.: +49 3501 530039<br />
+                            Fax: +49 3501 530022<br />
+                            Email: ralf.junghanns@tu-dresden.de<br />
                         </p>
                     </div>
                 </div>
