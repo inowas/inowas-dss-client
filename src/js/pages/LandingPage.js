3dd5c13f31783f1865b607904e41e36dd04581cf
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-03-12 19:48:43 +0100

    Tidy up Login and Navbar, Create LoginForm

--- src/js/pages/LandingPage.js
+++ src/js/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import '../../less/landingPage.less';
 
-import Login from '../components/landingPage/Login';
+import Navbar from './Navbar';
 import Footer from '../components/Footer';
 import Slider from '../components/primitive/Slider';
 import SliderItem from '../components/primitive/SliderItem';
@@ -14,8 +14,8 @@ export default class LandingPage extends React.Component {
     render( ) {
         return (
             <div className="application-wrapper landingPage-wrapper">
+                <Navbar />
                 <header className="header">
-                    <Login/>
                     <div className="app-width">
                         <Slider control={<SliderControl><SliderControlItem /><SliderControlItem /><SliderControlItem /><SliderControlItem /></SliderControl>}>
 
