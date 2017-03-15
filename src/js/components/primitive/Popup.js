49037fb27f077d70ec382d4affef47492e5f7528
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-03-15 14:47:05 +0100

    Add Lightbox to LandingPage

--- src/js/components/primitive/Popup.js
+++ src/js/components/primitive/Popup.js
@@ -10,7 +10,8 @@ export default class Popup extends Component {
         visible: PropTypes.bool,
         children: PropTypes.node,
         className: PropTypes.string,
-        close: PropTypes.func
+        close: PropTypes.func,
+        width: PropTypes.string
     }
 
     static defaultProps = {
@@ -18,11 +19,11 @@ export default class Popup extends Component {
     }
 
     render( ) {
-        const { visible, children, close, className } = this.props;
+        const { visible, children, close, className, width } = this.props;
         if ( visible ) {
             return (
                 <div className={'popup center-vertical center-horizontal' + ' ' + ( className || '' )}>
-                    <div className="content tile">
+                    <div className="content tile" style={{width}}>
                         <div className="header">
                             <button className="link" onClick={close}><Icon name="close"/></button>
                         </div>
