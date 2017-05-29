fb77ff2d4333b414646ed3e9abfb77df5169c46c
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-05-29 11:52:36 +0200

    Add functionality to add wells

--- src/containers/tools/T03Boundaries.jsx
+++ src/containers/tools/T03Boundaries.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import { setActiveBoundary, updateBoundary, setState } from '../../actions/T03';
+import { addBoundary, setActiveBoundary, setState, updateBoundary } from '../../actions/T03';
+import { maxBy, minBy } from 'lodash';
 
 import ConfiguredRadium from 'ConfiguredRadium';
 import FilterableList from '../../components/primitive/FilterableList';
@@ -8,8 +9,10 @@ import Tabs from '../../components/primitive/Tabs';
 import WellProperties from '../../components/modflow/WellProperties';
 import { connect } from 'react-redux';
 import { getActiveBoundary } from '../../reducers/T03/ui';
+import { getArea } from '../../reducers/T03/general';
 import { getBoundaries } from '../../reducers/T03/boundaries';
 import styleGlobals from 'styleGlobals';
+import uuid from 'uuid';
 
 const styles = {
     container: {
@@ -21,10 +24,17 @@ const styles = {
         display: 'flex'
     },
 
-    list: {
+    left: {
         flex: '1 1 40%',
+        maxWidth: '40%',
         borderRight: '1px solid ' + styleGlobals.colors.graySemilight,
-        marginRight: styleGlobals.dimensions.spacing.large
+        marginRight: styleGlobals.dimensions.spacing.large,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+
+    list: {
+        flex: 1
     },
 
     properties: {
@@ -38,10 +48,28 @@ class T03Boundaries extends Component {
     static propTypes = {
         style: PropTypes.object,
         boundary: PropTypes.string,
-        setActiveBoundary: PropTypes.func,
-        updateBoundary: PropTypes.func,
+        setActiveBoundary: PropTypes.func.isRequired,
+        updateBoundary: PropTypes.func.isRequired,
+        addBoundary: PropTypes.func.isRequired,
         boundaries: PropTypes.array,
-        setState: PropTypes.func
+        setState: PropTypes.func.isRequired,
+        area: PropTypes.array
+    }
+
+    addBoundary = type => {
+        return ( ) => {
+            // eslint-disable-next-line no-shadow
+            const { addBoundary, area } = this.props;
+
+            addBoundary({
+                name: 'New Boundary',
+                type,
+                optimisic: true,
+                id: uuid( ),
+                lng: ( minBy( area, 'lng' ).lng + maxBy( area, 'lng' ).lng ) / 2,
+                lat: ( minBy( area, 'lat' ).lat + maxBy( area, 'lat' ).lat ) / 2
+            });
+        };
     }
 
     renderProperties( activeBoundary ) {
@@ -67,23 +95,20 @@ class T03Boundaries extends Component {
     }
 
     render( ) {
-        const {
-            style, boundary,
-            // eslint-disable-next-line no-shadow
-            setActiveBoundary,
-            boundaries,
-            // eslint-disable-next-line no-shadow, no-unused-vars
-            updateBoundary,
-            ...rest
-        } = this.props;
+        // eslint-disable-next-line no-shadow
+        const { style, boundary, boundaries, setActiveBoundary } = this.props;
 
         const activeBoundary = boundaries.find(b => {
             return b.id === boundary;
         });
 
         return (
-            <div {...rest} style={[ style, styles.container ]}>
-                <FilterableList style={styles.list} clickAction={setActiveBoundary} list={boundaries}/>
+            <div style={[ style, styles.container ]}>
+                <div style={styles.left}>
+                    <FilterableList style={styles.list} clickAction={setActiveBoundary} list={boundaries}/>
+                    <button onClick={this.addBoundary( 'well' )} className="button">Add Well</button>
+                    <button onClick={this.addBoundary( 'river' )} className="button">Add River</button>
+                </div>
                 {activeBoundary === undefined || (
                     <div style={styles.properties}>
                         <h3>{activeBoundary.name}</h3>
@@ -98,11 +123,12 @@ class T03Boundaries extends Component {
 const mapStateToProps = state => {
     return {
         boundary: getActiveBoundary( state.T03.ui ),
-        boundaries: getBoundaries( state.T03.model.boundaries )
+        boundaries: getBoundaries( state.T03.model.boundaries ),
+        area: getArea( state.T03.model.general )
     };
 };
 
 // eslint-disable-next-line no-class-assign
-T03Boundaries = connect(mapStateToProps, { setActiveBoundary, updateBoundary, setState })( T03Boundaries );
+T03Boundaries = connect(mapStateToProps, { setActiveBoundary, updateBoundary, setState, addBoundary })( T03Boundaries );
 
 export default T03Boundaries;
