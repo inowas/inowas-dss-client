f7fe8e65da1d27b31e2de6f58b47dff75a23dda4
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-08-16 11:59:14 +0200

    Wire up inputs to state in modelEditorResultsHead

--- src/t03/selectors/index.js
+++ src/t03/selectors/index.js
@@ -1,9 +1,11 @@
 import * as model from './model';
 import * as general from './general';
 import * as boundary from './boundary';
+import * as results from './results';
 
 export {
     model,
     general,
     boundary,
+    results
 };
---
+++ src/t03/selectors/results.js
@@ -0,0 +1,3 @@
+export const getCalculationID = state => state ? state.calculation_id : null;
+export const getLayerValues = state => state ? state.layer_values : null;
+export const getTimes = state => state ? state.times : null;
