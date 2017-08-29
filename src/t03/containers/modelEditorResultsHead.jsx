1e8b579c25f6e06fe12c049668c35d0d00b1426f
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-08-29 08:59:00 +0200

    Bind mapPosition of ScenarioAnalysis to the state of modelEditorResults

--- src/t03/containers/modelEditorResultsHead.jsx
+++ src/t03/containers/modelEditorResultsHead.jsx
@@ -23,75 +23,120 @@ const styles = {
     },
 
     sliderWrapper: {
-        width: 4 * styleGlobals.dimensions.gridColumn + 3 * styleGlobals.dimensions.gridGutter,
+        width:
+            4 * styleGlobals.dimensions.gridColumn +
+            3 * styleGlobals.dimensions.gridGutter,
         padding: styleGlobals.dimensions.spacing.medium
     }
 };
 
 @ConfiguredRadium
 class ModelEditorResultsHead extends Component {
-
     static propTypes = {
         model: PropTypes.object,
         times: PropTypes.object,
         layerValues: PropTypes.array
-    }
+    };
 
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
 
         this.state = {
             selectedLayer: props.layerValues
-                ? `0_${ props.layerValues[0][0 ] }`
+                ? `0_${props.layerValues[0][0]}`
                 : null,
-            selectedTotalTimeIndex: 0
+            selectedTotalTimeIndex: 0,
+            mapPosition: {
+                bounds: [
+                    {
+                        lat: props.model.bounding_box[0][1],
+                        lng: props.model.bounding_box[0][0]
+                    },
+                    {
+                        lat: props.model.bounding_box[1][1],
+                        lng: props.model.bounding_box[1][0]
+                    }
+                ]
+            }
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (this.props.model !== nextProps.model) {
+            this.setState({
+                mapPosition: {
+                    bounds: [
+                        {
+                            lat: nextProps.model.bounding_box[0][1],
+                            lng: nextProps.model.bounding_box[0][0]
+                        },
+                        {
+                            lat: nextProps.model.bounding_box[1][1],
+                            lng: nextProps.model.bounding_box[1][0]
+                        }
+                    ]
+                }
+            });
+        }
+    }
+
+    setMapPosition = newMapPosition => {
+        this.setState({
+            mapPosition: newMapPosition
+        });
+    };
+
     onLayerSelectChange = nextSelectedLayer => {
         this.setState({
-            selectedLayer: nextSelectedLayer
-                ? nextSelectedLayer.value
-                : null
+            selectedLayer: nextSelectedLayer ? nextSelectedLayer.value : null
         });
-    }
+    };
 
     onTimeSliderChange = nextSelectedTotalTimeIndex => {
         this.setState({ selectedTotalTimeIndex: nextSelectedTotalTimeIndex });
-    }
+    };
 
-    render( ) {
+    render() {
         const { model, times, layerValues } = this.props;
-        const { selectedLayer, selectedTotalTimeIndex } = this.state;
+        const {
+            selectedLayer,
+            selectedTotalTimeIndex,
+            mapPosition
+        } = this.state;
 
-        if ( !model || !times || !layerValues ) {
-            return (
-                <div>
-                    Loading!
-                </div>
-            );
+        if (!model || !times || !layerValues) {
+            return <div>Loading!</div>;
         }
 
-        const options = layerValues.map(( l, index ) => ({
-            label: `Layer ${ index }`,
+        const options = layerValues.map((l, index) => ({
+            label: `Layer ${index}`,
             options: l
-                ? l.map(v => ({ label: `Layer ${ index } ${ v }`, value: `${ index }_${ v }` }))
-                : [ ]
+                ? l.map(v => ({
+                    label: `Layer ${index} ${v}`,
+                    value: `${index}_${v}`
+                }))
+                : []
         }));
 
-        const startDate = new Date( times.start_date_time );
+        const startDate = new Date(times.start_date_time);
         const totalTimes = times.total_times.map(t => {
-            return startDate.addDays( t );
+            return startDate.addDays(t);
         });
 
-        console.warn( model );
+        console.warn(model);
 
         const mapData = new ScenarioAnalysisMapData({
             area: model.geometry,
             grid: new Grid(
                 new BoundingBox(
-                    new Coordinate( model.bounding_box[0][1], model.bounding_box[0][0]),
-                    new Coordinate( model.bounding_box[1][1], model.bounding_box[1][0])
+                    new Coordinate(
+                        model.bounding_box[0][1],
+                        model.bounding_box[0][0]
+                    ),
+                    new Coordinate(
+                        model.bounding_box[1][1],
+                        model.bounding_box[1][0]
+                    )
                 ),
                 model.grid_size.n_x,
                 model.grid_size.n_y
@@ -99,30 +144,32 @@ class ModelEditorResultsHead extends Component {
             boundaries: model.boundaries
         });
 
-        const mapPosition = {
-            bounds: [
-                {
-                    lat: model.bounding_box[0][1],
-                    lng: model.bounding_box[0][0]
-                }, {
-                    lat: model.bounding_box[1][1],
-                    lng: model.bounding_box[1][0]
-                }
-            ]
-        };
-
         return (
             <div>
-                <div style={[ styles.selectWrapper ]}>
-                    <div style={[ styles.layerSelectWrapper ]}>
-                        <Select options={options} value={selectedLayer} onChange={this.onLayerSelectChange}/>
+                <div style={[styles.selectWrapper]}>
+                    <div style={[styles.layerSelectWrapper]}>
+                        <Select
+                            options={options}
+                            value={selectedLayer}
+                            onChange={this.onLayerSelectChange}
+                        />
                     </div>
-                    <div style={[ styles.sliderWrapper ]}>
-                        <ArraySlider data={totalTimes} value={selectedTotalTimeIndex} onChange={this.onTimeSliderChange} formatter={Formatter.dateToDate}/>
+                    <div style={[styles.sliderWrapper]}>
+                        <ArraySlider
+                            data={totalTimes}
+                            value={selectedTotalTimeIndex}
+                            onChange={this.onTimeSliderChange}
+                            formatter={Formatter.dateToDate}
+                        />
                     </div>
                 </div>
                 <div>
-                    <ScenarioAnalysisMap mapData={mapData} mapPosition={mapPosition} updateMapView={( ) => {}} updateBounds={( ) => {}} clickCoordinate={console.warn}/>
+                    <ScenarioAnalysisMap
+                        mapData={mapData}
+                        mapPosition={mapPosition}
+                        setMapPosition={this.setMapPosition}
+                        clickCoordinate={console.warn}
+                    />
                 </div>
             </div>
         );
@@ -132,10 +179,10 @@ class ModelEditorResultsHead extends Component {
 const mapStateToProps = (state, { tool }) => {
     return {
         model: model.getModflowModel(state[tool]),
-        calculationId: results.getCalculationID( state[tool].model.results ),
-        layerValues: results.getLayerValues( state[tool].model.results ),
-        times: results.getTimes( state[tool].model.results )
+        calculationId: results.getCalculationID(state[tool].model.results),
+        layerValues: results.getLayerValues(state[tool].model.results),
+        times: results.getTimes(state[tool].model.results)
     };
 };
 
-export default connect(mapStateToProps, { })( ModelEditorResultsHead );
+export default connect(mapStateToProps, {})(ModelEditorResultsHead);
