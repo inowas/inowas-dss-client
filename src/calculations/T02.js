48d6e78bdf59e9257fe321dfc6e9275163986bf3
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-14 09:31:09 +0200

    Prepare T02 for applying #1109

--- src/calculations/T02.js
+++ src/calculations/T02.js
@@ -1,18 +1,18 @@
 import numericallyIntegrate from './numericallyIntegrate';
 import erf from './erf';
 
-export function calculateDiagramData(variable, w, L, W, hi, Sy, K, t, x_min, x_max, d_x) {
-    const a = W / 2,
-        l = L / 2,
-        v = K * hi / Sy,
-        sqrt4vt = Math.sqrt(4 * v * t);
+export function calculateDiagramData(variable, w, L, W, hi, Sy, K, t, xMin, xMax, dX) {
+    const a = W / 2;
+    const l = L / 2;
+    const v = K * hi / Sy;
+    const sqrt4vt = Math.sqrt(4 * v * t);
 
     // eq 2
     function S(alpha, beta) {
         return numericallyIntegrate(0, 1, 0.001, function(tau) {
-            if (tau != 0) {
-                const sqrttau = Math.sqrt(tau);
-                return erf(alpha / sqrttau) * erf(beta / sqrttau);
+            if (tau !== 0) {
+                const sqrtTau = Math.sqrt(tau);
+                return erf(alpha / sqrtTau) * erf(beta / sqrtTau);
             }
             return 0;
         });
@@ -20,23 +20,28 @@ export function calculateDiagramData(variable, w, L, W, hi, Sy, K, t, x_min, x_m
 
     // code for eq 1 with y = 0
     function calcHhi(x, y) {
-        const s1 = S((l + x) / sqrt4vt, (a + y) / sqrt4vt),
-            s2 = S((l + x) / sqrt4vt, (a - y) / sqrt4vt),
-            s3 = S((l - x) / sqrt4vt, (a + y) / sqrt4vt),
-            s4 = S((l - x) / sqrt4vt, (a - y) / sqrt4vt);
-        return Math.sqrt(w / 2 / K * v * t * (s1 + s2 + s3 + s4) + hi * hi) - hi; // eq 1
+        const s1 = S((l + x) / sqrt4vt, (a + y) / sqrt4vt);
+        const s2 = S((l + x) / sqrt4vt, (a - y) / sqrt4vt);
+        const s3 = S((l - x) / sqrt4vt, (a + y) / sqrt4vt);
+        const s4 = S((l - x) / sqrt4vt, (a - y) / sqrt4vt);
+
+        if (hi === 0) {
+            return Math.sqrt(w / 2 / K * v * t * (s1 + s2 + s3 + s4) + hi * hi) - hi;
+        }
+
+        return Math.sqrt(w / 2 / K * v * t * (s1 + s2 + s3 + s4) + hi * hi) - hi;
     }
 
-    let data = [];
-    if (variable == 'x') {
-        for (let x = x_min; x < x_max; x += d_x) {
+    const data = [];
+    if (variable === 'x') {
+        for (let x = xMin; x < xMax; x += dX) {
             data.push({
                 x,
                 hhi: calcHhi(x, 0)
             });
         }
     } else {
-        for (let y = x_min; y < x_max; y += d_x) {
+        for (let y = xMin; y < xMax; y += dX) {
             data.push({
                 y,
                 hhi: calcHhi(0, y)
