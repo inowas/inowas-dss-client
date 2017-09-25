adec81cefb62283f9c5b0e3a23e6a6054c0a6a38
Ralf Junghanns <ralf.junghanns@gmail.com>
2017-09-25 14:05:21 +0200

    Add rbf to well-styles

--- src/components/modflow/EditableWells.jsx
+++ src/components/modflow/EditableWells.jsx
@@ -45,6 +45,13 @@ const styles = {
         fillColor: 'darkblue',
         fillOpacity: 0.7
     },
+    rbf: {
+        radius: 5,
+        color: 'black',
+        weight: 1,
+        fillColor: 'yellow',
+        fillOpacity: 1
+    },
     smw: {
         radius: 5,
         color: 'black',
