c33dc1d1c23eb3a19a5f3ba63caad2c7cfb3d6ce
Ramandeep Jain <ramandeepjain@gmail.com>
2017-12-20 13:51:58 +0100

    T09E: constant head implemented

--- src/t09/containers/T09.js
+++ src/t09/containers/T09.js
@@ -9,7 +9,7 @@ import image9A from '../../images/tools/T09A.png';
 import image9B from '../../images/tools/T09B.png';
 import image9C from '../../images/tools/T09C.png';
 import image9D from '../../images/tools/T09D.png';
-// import image9E from '../../images/tools/T09E.png';
+import image9E from '../../images/tools/T09E.png';
 
 import Icon from '../../components/primitive/Icon';
 import Navbar from '../../containers/Navbar';
@@ -100,6 +100,18 @@ class T09 extends React.Component {
                             </p>
                             <div className="center-horizontal center-vertical">
                                 <img className="sketch-image" src={image9D}/>
+                            </div>
+                        </div>
+                    </a>
+                    <a style={{'cursor': 'pointer'}} onClick={() => this.redirectTo('T09E')}
+                       className="tile col col-rel-1-t13">
+                        <div className="div-block">
+                            <h1>T09E</h1>
+                            <p className="p-height">
+                                Sea level rise
+                            </p>
+                            <div className="center-horizontal center-vertical">
+                                <img className="sketch-image" src={image9E}/>
                             </div>
                         </div>
                     </a>
