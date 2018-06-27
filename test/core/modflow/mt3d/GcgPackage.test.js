4ea75bb1d830513ced1bb12c80196c157828b27c
Ralf Junghanns <ralf.junghanns@gmail.com>
2018-06-27 08:32:35 +0200

    Implement mt3dms GcgPackage

---
+++ test/core/modflow/mt3d/GcgPackage.test.js
@@ -0,0 +1,34 @@
+import GcgPackage from '../../../../src/core/modflow/mt3d/gcgPackage';
+
+test('Get GcgPackage from Default', () => {
+    const gcg = GcgPackage.fromDefault();
+    expect(gcg).toBeInstanceOf(GcgPackage);
+    expect(gcg.packageName).toEqual('gcg');
+    expect(gcg.mxiter).toEqual(1);
+    expect(gcg.iter1).toEqual(50);
+    expect(gcg.isolve).toEqual(3);
+    expect(gcg.ncrs).toEqual(0);
+    expect(gcg.accl).toEqual(1);
+    expect(gcg.cclose).toEqual(1e-5);
+    expect(gcg.iprgcg).toEqual(0);
+    expect(gcg.extension).toEqual('gcg');
+    expect(gcg.unitnumber).toEqual(null);
+    expect(gcg.filenames).toEqual(null);
+});
+
+test('Get GcgPackage from Object', () => {
+    const gcgObj = {
+        mxiter: 1,
+        iter1: 50,
+        isolve: 3,
+        ncrs: 0,
+        accl: 1,
+        cclose: 1e-5,
+        iprgcg: 0,
+        extension: 'gcg',
+        unitnumber: null,
+        filenames: null
+    };
+    const gcg = GcgPackage.fromObject(gcgObj);
+    expect(gcg.toObject).toEqual(gcgObj);
+});
