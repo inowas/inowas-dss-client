b6f4b105864bf9f4070e96d4fbd7acf3eb0ca5ed
Ralf Junghanns <ralf.junghanns@gmail.com>
2018-09-11 22:29:21 +0900

    Bugfix Calibrationstate

--- src/t03/components/runModel/CalculationStatus.jsx
+++ src/t03/components/runModel/CalculationStatus.jsx
@@ -2,14 +2,17 @@ import ConfiguredRadium from 'ConfiguredRadium';
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Progress} from 'semantic-ui-react';
+import * as Selector from '../../selectors';
 
 const status = ({calculation}) => (
-    <Progress value={calculation.state} total={4} success={calculation.state === 4}>
-        {calculation.state === 0 && ''}
-        {calculation.state === 1 && 'Preprocessing...'}
-        {calculation.state === 2 && 'Calculation queued...'}
-        {calculation.state === 3 && 'Calculating...'}
-        {calculation.state === 4 && 'Calculation finished.'}
+    <Progress value={calculation.state} total={6} success={calculation.state === 6}>
+        {calculation.state === Selector.model.CALCULATION_STATE_NEW && ''}
+        {calculation.state === Selector.model.CALCULATION_STATE_STARTED && 'Started...'}
+        {calculation.state === Selector.model.CALCULATION_STATE_PREPROCESSING && 'Preprocessing...'}
+        {calculation.state === Selector.model.CALCULATION_STATE_PREPROCESSING_FINISHED && 'Preprocessing finished...'}
+        {calculation.state === Selector.model.CALCULATION_STATE_QUEUED && 'Queued...'}
+        {calculation.state === Selector.model.CALCULATION_STATE_CALCULATING && 'Calculating...'}
+        {calculation.state === Selector.model.CALCULATION_STATE_FINISHED && 'Calculation finished!'}
     </Progress>
 );
 
